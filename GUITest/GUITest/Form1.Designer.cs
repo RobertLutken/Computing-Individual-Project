90747b967ce4e0e5112421d855519169b1a595b4
RobertLutken <rolandroid117@gmail.com>
2013-11-06 00:00:54 +0000

    Graphical User Interface Testing

---
+++ GUITest/GUITest/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GUITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btRun = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbTimer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btRun
+            // 
+            this.btRun.Location = new System.Drawing.Point(851, 565);
+            this.btRun.Name = "btRun";
+            this.btRun.Size = new System.Drawing.Size(132, 51);
+            this.btRun.TabIndex = 0;
+            this.btRun.Text = "Run ->\r\n";
+            this.btRun.UseVisualStyleBackColor = true;
+            this.btRun.Click += new System.EventHandler(this.btRun_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 565);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<- Back \r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 76);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Objective :\r\nDisplay hello world to the consle !";
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.Font = new System.Drawing.Font("Tw Cen MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimer.Location = new System.Drawing.Point(681, 9);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(183, 76);
+            this.lbTimer.TabIndex = 3;
+            this.lbTimer.Text = "Time Remaining  \r\n         00:10";
+            this.lbTimer.Click += new System.EventHandler(this.lbTimer_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tw Cen MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(876, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 76);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Score \r\n   0\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BackColor = System.Drawing.Color.Navy;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(49, 153);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(901, 374);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Python >>  print hello world";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tw Cen MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 38);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Python Code :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(995, 643);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbTimer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btRun);
+            this.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Level 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRun;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbTimer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GUITest/GUITest/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+namespace GUITest
+{
+    public partial class Form1 : Form
+    {
+        //Label lbTimer1 = new Label();
+        public Form1()
+        {
+            InitializeComponent();
+            lbTimer.Update();
+            lbTimer.Refresh();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Tab))
+            {
+                textBox1.Focus();
+            }
+        }
+        class mTimer :  Form1
+        {
+            DateTime mTime;
+
+            public mTimer(int minutes) { mTime = DateTime.Now.AddMinutes(minutes); startTimer(); }
+            // public static void setTime(int minutes) { mTimer m2 = new mTimer(); m2.mTime = DateTime.Now.AddMinutes(minutes); m2.startTimer(); }
+
+            // private static void setTime(int hours , int minutes, int seconds) { mTimer m2 = new mTimer(); m2.mTime = DateTime.Now.AddMinutes(hours); m2.startTimer(); }
+
+            public void startTimer()
+            {
+                // Create a new timer
+                System.Timers.Timer t = new System.Timers.Timer();
+                // set the interval of the timer to 1 sec
+                t.Interval = 1000;
+
+                t.Elapsed += new ElapsedEventHandler(onTimerTick);
+                TimeSpan ts = mTime.Subtract(DateTime.Now);
+                t.Start();
+            }
+
+            void onTimerTick(object source, ElapsedEventArgs e)
+            {
+                TimeSpan ts = mTime.Subtract(DateTime.Now);
+
+                Console.Write("\rYou have  : " + ts.Minutes + " Minutes and " + ts.Seconds + " Seconds remaining.");
+               lbTimer.Text = "Time Remaining " + ts.Minutes + "Minutes and" + ts.Seconds + "Seconds remaining.";
+           //    lbTimer.Refresh();
+               lbTimer.Update();
+            }
+          public  string getText()
+            {
+                TimeSpan ts = mTime.Subtract(DateTime.Now);
+                return "Time Remaining " + ts.Minutes + "Minutes and" + ts.Seconds + "Seconds remaining.";
+            }
+        }
+
+        private void btRun_Click(object sender, EventArgs e)
+        {
+            mTimer m = new mTimer(10);
+            lbTimer.Text = m.getText();
+            lbTimer.Refresh();
+        }
+
+        private void lbTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void lbTimer_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+      //  public virtual void Refresh() { }
+    }
+}
